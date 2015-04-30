00ac45638ed5b7ea3409ac6c0e5ea34e26027b60
dululu522 <1124955669@qq.com>
2015-04-30 23:57:31 +0800

    xxx

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by 堵路路 on 15/4/30.
+//  Copyright (c) 2015年 堵路路. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by 堵路路 on 15/4/30.
+//  Copyright (c) 2015年 堵路路. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
